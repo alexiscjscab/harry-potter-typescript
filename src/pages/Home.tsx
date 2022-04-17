b9b4d1badf67367e70d591b975a5da5e9450b34f
alexiscjscab <alexiscjscab@gmail.com>
2022-04-17 20:56:23 -0300

    Add loader snipper

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,16 @@
-import { useState, useEffect } from 'react';
-import Cards from '../components/Cards/Cards';
-import Search from '../components/Search/Search';
-import { Character, CharacterMap } from '../types/types';
-import './homeStyles.scss'
+import { useState, useEffect } from "react";
+import Cards from "../components/Cards/Cards";
+import Search from "../components/Search/Search";
+import { Character, CharacterMap } from "../types/types";
+import "./homeStyles.scss";
+import { Grid } from "react-loader-spinner";
 
-
-const Home = (): JSX.Element => {
+const Home = () => {
   const [characterList, setCharacterList] = useState<Character[]>([]);
   const [originalList, setOriginalList] = useState<Character[]>([]);
   useEffect(() => {
     const fetchCharacterApi = async (): Promise<CharacterMap> => {
-      return fetch('https://hp-api.herokuapp.com/api/characters').then(
+      return fetch("https://hp-api.herokuapp.com/api/characters").then(
         (response) => response.json()
       );
     };
@@ -21,7 +21,10 @@ const Home = (): JSX.Element => {
           name: character.name,
           nickName: character.alternate_names,
           species: character.species,
-          img: index <= 25 ? character.image : 'https://wallpaperaccess.com/full/635136.jpg',
+          img:
+            index <= 25
+              ? character.image
+              : "https://wallpaperaccess.com/full/635136.jpg",
           gender: character.gender,
           house: character.house,
           alive: character.alive,
@@ -43,13 +46,20 @@ const Home = (): JSX.Element => {
       return character.name.toLowerCase().includes(name.toLowerCase());
     });
     setCharacterList(filteredList);
-  }
-
+  };
 
   return (
-    <div className='home'>
-      <Search search={searchName}/>
-      <Cards character={characterList} />
+    <div className="home">
+      <Search search={searchName} />
+      {characterList.length > 0 ? (
+        <>
+          <Cards character={characterList} />
+        </>
+      ) : (
+        <div className="loading">
+          <Grid height='100' width='100' color='#000' ariaLabel='loading' />
+        </div>
+      )}
     </div>
   );
 };
